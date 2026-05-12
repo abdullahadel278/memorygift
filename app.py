05a1c81f33b942f089ca3f2bf7c3df0b0a13c1e0
abdullahadel278 <abdullah.adel278@gmail.com>
2026-05-12 01:32:14 +0000

    sqlite simple

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db')
 
 db = SQLAlchemy(app)
 
